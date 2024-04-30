5d1303d12eb6f62712cbffdf16bcde9a61a2c021
elite_lin <elite_lin@tw-futuba.com.tw>
2024-04-30 14:35:03 +0800

    add fun SearchBar()

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/TextFieldExample.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/TextFieldExample.kt
@@ -1,20 +1,31 @@
 package com.gmail.vexonelite.jetpack.study
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,21 +33,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.gmail.vexonelite.jetpack.study.ui.theme.Green001
 import com.gmail.vexonelite.jetpack.study.ui.theme.Pink001
-import com.gmail.vexonelite.jetpack.study.viewmodels.ListViewModel
 import com.gmail.vexonelite.jetpack.study.viewmodels.SignUpViewModel
 
 
@@ -228,3 +244,99 @@ fun FocusDemo01() {
 }
 
 
+/**
+ * * [Ref](https://rivaldy.medium.com/jetpack-compose-customize-your-searchbar-with-basictextfield-c1cdcbd3e3aa)
+ */
+@Composable
+fun SearchBar(
+    hint: String,
+    modifier: Modifier = Modifier,
+    isEnabled: (Boolean) = true,
+    height: Dp = 40.dp,
+    elevation: Dp = 4.dp,
+    cornerShape: Shape = RoundedCornerShape(8.dp),
+    backgroundColor: Color = Color.White,
+    onSearchClicked: () -> Unit = {},
+    onTextChange: (String) -> Unit = {},
+) {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+    Row(
+        modifier = Modifier
+            .height(height)
+            .fillMaxWidth()
+            .shadow(elevation = elevation, shape = cornerShape)
+            .background(color = backgroundColor, shape = cornerShape)
+            .clickable { onSearchClicked() },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        BasicTextField(
+            modifier = modifier
+                .weight(5f)
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            value = text,
+            onValueChange = {
+                text = it
+                onTextChange(it.text)
+            },
+            enabled = isEnabled,
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            ),
+            decorationBox = { innerTextField ->
+                if (text.text.isEmpty()) {
+                    Text(
+                        text = hint,
+                        color = Color.Gray.copy(alpha = 0.5f),
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+                innerTextField()
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(onSearch = { onSearchClicked() }),
+            singleLine = true
+        )
+        Box(
+            modifier = modifier
+                .weight(1f)
+                .size(40.dp)
+                .background(color = Color.Transparent, shape = CircleShape)
+                .clickable {
+                    if (text.text.isNotEmpty()) {
+                        text = TextFieldValue(text = "")
+                        onTextChange("")
+                    }
+                },
+        ) {
+            if (text.text.isNotEmpty()) {
+                Icon(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    imageVector = Icons.Rounded.Clear,
+                    //contentDescription = stringResource(R.string.search),
+                    contentDescription = "search",
+                    tint = MaterialTheme.colorScheme.primary,
+                )
+            } else {
+                Icon(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    //painter = painterResource(id = R.drawable.ic_search),
+                    imageVector = Icons.Rounded.Search,
+                    //contentDescription = stringResource(R.string.search),
+                    contentDescription = "search",
+                    tint = MaterialTheme.colorScheme.primary,
+                )
+            }
+        }
+    }
+}
