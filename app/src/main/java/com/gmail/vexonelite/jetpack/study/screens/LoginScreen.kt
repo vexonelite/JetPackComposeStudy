3789ae3094c02ea3c6eb647fe64c8b8ac9650925
elite_lin <elite_lin@tw-futuba.com.tw>
2024-05-10 16:45:53 +0800

    refine

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/LoginScreen.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/LoginScreen.kt
@@ -10,24 +10,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -42,8 +35,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -51,44 +42,17 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.gmail.vexonelite.jetpack.study.ProgressDialog
-import com.gmail.vexonelite.jetpack.study.RouteDestination
 import com.gmail.vexonelite.jetpack.study.SearchBar
 import com.gmail.vexonelite.jetpack.study.ui.theme.Blue003
 import com.gmail.vexonelite.jetpack.study.ui.theme.Green001
-import com.gmail.vexonelite.jetpack.study.ui.theme.Grey80
-import com.gmail.vexonelite.jetpack.study.ui.theme.HoloBlueDark
 import com.gmail.vexonelite.jetpack.study.ui.theme.Pink001
-import com.gmail.vexonelite.jetpack.study.ui.theme.navigateToExt
-import com.gmail.vexonelite.jetpack.study.viewmodels.FmBuiltInSingleActionDialog
-import com.gmail.vexonelite.jetpack.study.viewmodels.FmBuiltInTwinActionsDialog
-import com.gmail.vexonelite.jetpack.study.viewmodels.FmProgressDialog
 import com.gmail.vexonelite.jetpack.study.viewmodels.FmTextField01
 import com.gmail.vexonelite.jetpack.study.viewmodels.FmTextField02
-import com.gmail.vexonelite.jetpack.study.viewmodels.ImmutableObjectList
-import com.gmail.vexonelite.jetpack.study.viewmodels.ListViewModel
-
-import java.util.logging.Level
-import java.util.logging.Logger
 
 
 @Composable
 fun LoginScreen01(
-    progressDialogState: State<Boolean>,
-    singleActionDialogState: State<Boolean>,
-    twinActionsDialogState: State<Boolean>,
     onLoginButtonClick: () -> Unit,
-    onProgressDialogDismiss: () -> Unit,
-    onSingleActionDialogConfirmClick: () -> Unit,
-    onSingleActionDialogDismiss: () -> Unit,
-    onTwinActionsDialogConfirmClick: () -> Unit,
-    onTwinActionsDialogDismiss: () -> Unit,
-    progressDialogTitle: String = "",
-    singleActionDialogTitle: String = "",
-    singleActionDialogMessage: String = "",
-    twinActionsDialogTitle: String = "",
-    twinActionsDialogMessage: String = "",
 ) {
     val accountFocusRequester: FocusRequester = remember { FocusRequester() }
     val pwdFocusRequester: FocusRequester = remember { FocusRequester() }
@@ -110,6 +74,10 @@ fun LoginScreen01(
             //maxLines = 1,
             //modifier = Modifier.fillMaxWidth().background(Grey80).padding(vertical = 12.dp)
         )
+
+        Spacer(modifier = Modifier.padding(vertical = 10.dp))
+
+
 
         Spacer(modifier = Modifier.padding(vertical = 10.dp))
 
@@ -155,27 +123,7 @@ fun LoginScreen01(
         }
     }
 
-    FmProgressDialog(
-        title = progressDialogTitle,
-        dialogState = progressDialogState,
-        onDismiss = onProgressDialogDismiss,
-    )
 
-    FmBuiltInSingleActionDialog(
-        dialogState = singleActionDialogState,
-        onConfirm = onSingleActionDialogConfirmClick,
-        onDismiss = onSingleActionDialogDismiss,
-        title = singleActionDialogTitle,
-        message = singleActionDialogMessage,
-    )
-
-    FmBuiltInTwinActionsDialog(
-        dialogState = twinActionsDialogState,
-        onConfirm = onTwinActionsDialogConfirmClick,
-        onDismiss = onTwinActionsDialogDismiss,
-        title = twinActionsDialogTitle,
-        message = twinActionsDialogMessage,
-    )
 }
 
 
