226fa3672d05708e46f2e4247158e02332750bd6
elite_lin <elite_lin@tw-futuba.com.tw>
2024-05-10 16:45:05 +0800

    refine

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/AppUiColors.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/AppUiColors.kt
@@ -1,10 +1,18 @@
 package com.gmail.vexonelite.jetpack.study.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.gmail.vexonelite.jetpack.study.ui.theme.Blue001
 import com.gmail.vexonelite.jetpack.study.ui.theme.Blue008
 import com.gmail.vexonelite.jetpack.study.ui.theme.Green001
@@ -13,7 +21,6 @@ import com.gmail.vexonelite.jetpack.study.ui.theme.Grey27
 import com.gmail.vexonelite.jetpack.study.ui.theme.HoloBlueDark
 import com.gmail.vexonelite.jetpack.study.ui.theme.Pink001
 import com.gmail.vexonelite.jetpack.study.ui.theme.Pink002
-
 
 
 fun theAppButtonColor01(): ButtonColors
