96ee320cec2cf479b22fd1e9d909a6a5c8bf1457
elite_lin <elite_lin@tw-futuba.com.tw>
2024-05-10 16:41:56 +0800

    refine

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/MenuScreen.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/MenuScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -14,6 +15,8 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -29,10 +32,12 @@ import com.gmail.vexonelite.jetpack.study.ArticleItemDefault
 import com.gmail.vexonelite.jetpack.study.HolderItemClickDelegate
 import com.gmail.vexonelite.jetpack.study.R
 import com.gmail.vexonelite.jetpack.study.ui.theme.Grey80
+import com.gmail.vexonelite.jetpack.study.viewmodels.AspectRatioReference
 import com.gmail.vexonelite.jetpack.study.viewmodels.ImmutableObjectList
 import com.gmail.vexonelite.jetpack.study.viewmodels.ListViewModel
 import com.gmail.vexonelite.jetpack.study.viewmodels.MenuItemContentType
 import com.gmail.vexonelite.jetpack.study.viewmodels.MenuItemModel
+import com.gmail.vexonelite.jetpack.study.viewmodels.aspectRatioReference
 import com.gmail.vexonelite.jetpack.study.viewmodels.generateType1List
 import com.gmail.vexonelite.jetpack.study.viewmodels.generateType2List
 import java.util.logging.Level
@@ -85,7 +90,14 @@ fun ColorMenuItemGrid01(menuItem: MenuItemModel, itemClickCallback: HolderItemCl
             modifier = Modifier.fillMaxWidth(),
         ) {
             Surface(
-                modifier = Modifier.aspectRatio(ratio = 1f).fillMaxWidth(),
+                modifier = Modifier
+                    //.aspectRatio(ratio = 1f)
+                    .aspectRatioReference(
+                        ratioWidth = 16f,
+                        ratioHeight = 9f,
+                        reference = AspectRatioReference.MIN_PARENT_WIDTH_PARENT_HEIGHT,
+                    ),
+                    //.fillMaxWidth(),
                 color = menuItem.color,
                 //shape = RoundedCornerShape(10.dp),
                 //border = BorderStroke(2.dp, Blue007),
@@ -113,19 +125,44 @@ fun ImageMenuItemGrid01(menuItem: MenuItemModel, itemClickCallback: HolderItemCl
         Column(
             modifier = Modifier.fillMaxWidth(),
         ) {
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(menuItem.action)
-                    .crossfade(true)
-                    .build(),
-                // java.lang.IllegalArgumentException: Only VectorDrawables and rasterized asset types are supported ex. PNG, JPG, WEBP
-                //cannot use xml drawable!!
-                //placeholder = painterResource(R.drawable.rect_gray_01),
-                contentDescription = stringResource(R.string.image_description),
-                //contentScale = ContentScale.Crop,
-                //modifier = Modifier.clip(CircleShape)
-                modifier = Modifier.aspectRatio(ratio = 1.333f).fillMaxWidth(),
-            )
+            Box( // Child
+                modifier = Modifier
+                    .aspectRatioReference(
+                        ratioWidth = 1f,
+                        ratioHeight = 1f,
+                        AspectRatioReference.MIN_PARENT_WIDTH_PARENT_HEIGHT
+                    ),
+                    //.align(Alignment.Center),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(menuItem.action)
+                        .crossfade(true)
+                        .build(),
+                    // java.lang.IllegalArgumentException: Only VectorDrawables and rasterized asset types are supported ex. PNG, JPG, WEBP
+                    //cannot use xml drawable!!
+                    //placeholder = painterResource(R.drawable.rect_gray_01),
+                    contentDescription = stringResource(R.string.image_description),
+                    //contentScale = ContentScale.Crop,
+                    //modifier = Modifier.clip(CircleShape)
+                    modifier = Modifier.fillMaxSize(),
+                )
+            }
+//            AsyncImage(
+//                model = ImageRequest.Builder(LocalContext.current)
+//                    .data(menuItem.action)
+//                    .crossfade(true)
+//                    .build(),
+//                // java.lang.IllegalArgumentException: Only VectorDrawables and rasterized asset types are supported ex. PNG, JPG, WEBP
+//                //cannot use xml drawable!!
+//                //placeholder = painterResource(R.drawable.rect_gray_01),
+//                contentDescription = stringResource(R.string.image_description),
+//                //contentScale = ContentScale.Crop,
+//                //modifier = Modifier.clip(CircleShape)
+//                modifier = Modifier.aspectRatio(ratio = 1.333f).fillMaxWidth(),
+//            )
+
             Text(
                 text = menuItem.description,
                 fontSize = 20.sp,
