9c7aafd28791c27173f3fc83c802a0573c0804af
elite_lin <elite_lin@tw-futuba.com.tw>
2024-05-10 16:42:09 +0800

    add sample

---
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/StateExample.kt
@@ -0,0 +1,29 @@
+package com.gmail.vexonelite.jetpack.study
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+private fun HelloContent() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Hello!",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Name") }
+        )
+    }
+}
+
