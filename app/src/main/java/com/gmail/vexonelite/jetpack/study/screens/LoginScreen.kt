ea4338ead3c610b5be65d36cb6212d835a1c9c41
elite_lin <elite_lin@tw-futuba.com.tw>
2024-06-05 10:51:27 +0800

    minor

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/LoginScreen.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/screens/LoginScreen.kt
@@ -2,10 +2,6 @@ package com.gmail.vexonelite.jetpack.study.screens
 
 
 import android.widget.Toast
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -27,19 +23,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gmail.vexonelite.jetpack.study.SearchBar
@@ -50,9 +44,10 @@ import com.gmail.vexonelite.jetpack.study.viewmodels.FmTextField01
 import com.gmail.vexonelite.jetpack.study.viewmodels.FmTextField02
 
 
+@Preview
 @Composable
-fun LoginScreen01(
-    onLoginButtonClick: () -> Unit,
+fun LoginScreenContent(
+    onLoginButtonClick: () -> Unit = {},
 ) {
     val accountFocusRequester: FocusRequester = remember { FocusRequester() }
     val pwdFocusRequester: FocusRequester = remember { FocusRequester() }
