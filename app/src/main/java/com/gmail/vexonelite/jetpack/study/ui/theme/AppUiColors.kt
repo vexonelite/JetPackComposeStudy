a56a0d24f059daf396050a08564d8f057c16025c
elite_lin <elite_lin@tw-futuba.com.tw>
2024-06-20 17:33:25 +0800

    add fun DoubleUpCircle01() {}

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/ui/theme/AppUiColors.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/ui/theme/AppUiColors.kt
@@ -1,14 +1,25 @@
 package com.gmail.vexonelite.jetpack.study.ui.theme
 
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardDoubleArrowUp
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.gmail.vexonelite.jetpack.study.ui.theme.Blue001
 import com.gmail.vexonelite.jetpack.study.ui.theme.Grey136
 import com.gmail.vexonelite.jetpack.study.ui.theme.HoloBlueDark
@@ -100,4 +111,30 @@ fun theBuiltInCheckboxColor02(): CheckboxColors =
         //uncheckedBoxColor = Blue009,
     )
 
+
+@Preview
+@Composable
+fun DoubleUpCircle01(
+    arcColor: Color = Teal200,
+    size: Dp = 200.dp,
+) {
+    Box(
+        modifier = Modifier.size(size)
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            drawCircle(
+                color = arcColor,
+            )
+        }
+
+        Icon(
+            modifier = Modifier.fillMaxSize(),
+            imageVector = Icons.Filled.KeyboardDoubleArrowUp,
+            tint = Color.White,
+            contentDescription = null,
+        )
+    }
+}
 
