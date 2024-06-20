0257a1e4594e303c179bc0828bcc0cee8fa7d6d4
elite_lin <elite_lin@tw-futuba.com.tw>
2024-06-20 17:33:35 +0800

    add fun HalfCircle() {}

--- app/src/main/java/com/gmail/vexonelite/jetpack/study/CanvasExamples.kt
+++ app/src/main/java/com/gmail/vexonelite/jetpack/study/CanvasExamples.kt
@@ -1,2 +1,31 @@
 package com.gmail.vexonelite.jetpack.study
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gmail.vexonelite.jetpack.study.ui.theme.Teal200
+
+
+@Preview
+@Composable
+fun HalfCircle(
+    arcColor: Color = Teal200
+) {
+    Canvas(
+        modifier = Modifier
+            .size(200.dp)
+
+    ) {
+        drawArc(
+            color = arcColor,
+            startAngle = 180f,
+            sweepAngle = 180f,
+            useCenter = true
+        )
+    }
+}
+
